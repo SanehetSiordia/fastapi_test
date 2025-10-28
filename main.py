595e2d7c222dd5a8e76eb6154fb093c52ba06371
SinhueSiordia <carpediem.sinhue@gmail.com>
2025-10-28 11:43:03 -0600

    respuesta json

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Body, Path, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
 import datetime
@@ -53,41 +53,44 @@ movies : List[Movie] = []
 
 @app.get('/movies', tags=['Home'])
 def get_movies()->List[Movie]:
-    return [movie.model_dump() for movie in movies]
-
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
+    
 #PATH PARAM
 @app.get('/movies/{id}', tags=['Movies'])
 def read_movie(id: int = Path(gt=0))->Movie | dict:
     for movie in movies:
         if movie.id==id:
-            return movie.model_dump()
-    return {}
+            return JSONResponse(content=movie.model_dump())
+    return JSONResponse(content={})
 
 #QUERY PARAM
 @app.get('/movies/', tags=['Movies'])
 def get_movie_by_category(category: str = Query(min_length=5, max_length=20))->Movie | dict:
     for movie in movies:
         if movie.category==category:
-            return movie.model_dump()
-    return {}
+            return JSONResponse(content=movie.model_dump())
+    return  JSONResponse(content={})
 
 #POST METHOD
 @app.post('/movies', tags=['Movies'])
 def create_movie(movie:MovieCreate)->List[Movie]:
     movies.append(movie) #Se registra solo el objeto
-    return [movie.model_dump() for movie in movies]
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
 
 #PUT METHOD
 @app.put('/movies/{id}', tags=['Movies'])
 def update_movie(id: int,movie:MovieUpdate)->List[Movie]:
     for item in movies:
-        if item['id']==id:
-            item['title']=movie.title
-            item['overview']=movie.overview
-            item['year']=movie.year
-            item['raiting']=movie.raiting
-            item['category']=movie.category
-    return [movie.model_dump() for movie in movies]
+        if item.id==id:
+            item.title=movie.title
+            item.overview=movie.overview
+            item.year=movie.year
+            item.raiting=movie.raiting
+            item.category=movie.category
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
 
 #DELETE METHOD
 @app.delete('/movies/{id}', tags=['Movies'])
@@ -95,4 +98,5 @@ def delete_movie(id:int)->List[Movie] | dict:
     for movie in movies:
         if movie.id==id:
             movies.remove(movie)
-    return [movie.model_dump() for movie in movies]
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
