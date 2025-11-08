5787a088edc3b6775a3db4137a1ca012abf0ef1a
SinhueSiordia <carpediem.sinhue@gmail.com>
2025-11-08 11:05:09 -0600

    se agrega estructura con dependencias

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Request, Response, status
-from fastapi.responses import JSONResponse, PlainTextResponse
+import os
+from fastapi import FastAPI, Request, Depends
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from typing import Annotated
 from src.routers.movie_router import movie_router
 from src.utils.http_error_handler import HTTPErrorHandler
 
@@ -8,21 +11,49 @@ app = FastAPI()
 app.title ="HolaMundo fastAPI SSM"
 app.version= "0.0.1-SNAPSHOT"
 
-#Importar middleware para manejo de errores
-#app.middleware(HTTPErrorHandler) #Starlette
-@app.middleware('http')
-async def http_error_handler(request:Request, call_next) -> Response | JSONResponse:
-    print('Middleware is running!')
-    try:
-        return await call_next(request)    
-    except Exception as e:
-        content = f"ex: {str(e)}"
-        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return JSONResponse(content=content, status_code=status_code)
+#Importar middleware para manejo de errores con Starlette
+app.middleware(HTTPErrorHandler) 
+
+#Rutas para estilos y templates de plantillas html
+static_path = os.path.join(os.path.dirname(__file__), 'static/')
+templates_path = os.path.join(os.path.dirname(__file__), 'templates/')
+
+#Carga de archivos de ruta espeficia
+app.mount('/static', StaticFiles(directory=static_path),'static')
+templates= Jinja2Templates(directory=templates_path)
 
 @app.get('/', tags=['Home'])
-def home():
-    return PlainTextResponse(content='Home')
+def home(request:Request):
+    #Renderizado al iniciar
+    return templates.TemplateResponse('index.html',{'request':request,'message':'Welcome'})
+
+#Inyeccion de dependencias
+#Tipo 1 con metodo y libreria FastApi - Depends
+def commons_param(start_date:str, end_date:str):
+    return {"start_date":start_date,"end_date":end_date}
+
+#Tipo 2 con Annotated de Typing
+commons_dependency=Annotated[dict, Depends(commons_param)]
+
+#Tipo 3 con Clase
+class CommonDependency:
+    def __init__(self,start_date: str, end_date: str) -> None:
+        self.start_date = start_date
+        self.end_date = end_date
+
+@app.get('/users', tags=['venta'])
+def get_users(commons: commons_dependency):    #dependencia declarada con Annotated de typing
+    return f"Users created between {commons['start_date']} and {commons['end_date']}"
+
+@app.get('/customers', tags=['venta'])
+def get_customers(commons: dict=Depends(commons_param)):    #dependencia guardada como variable diccionario
+    return f"Customers created between {commons['start_date']} and {commons['end_date']}"
+
+
+@app.get('/employees', tags=['venta'])
+def get_users(commons: CommonDependency=Depends()):    #dependencia declarada con una clase (mismo nombre de variable igualada, se puede eliminar)
+    return f"Employee created between {commons.start_date} and {commons.end_date}"
+
 
 #Creacion de las rutas en MAIN
 app.include_router(prefix='/movies', router=movie_router)
