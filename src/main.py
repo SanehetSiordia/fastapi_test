6f9d652c448d0cedf52b09c903676b2ab88f9705
SinhueSiordia <carpediem.sinhue@gmail.com>
2025-11-08 11:02:55 -0600

    depdencias

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, Request, Depends, Query
+from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import Annotated
@@ -28,18 +28,32 @@ def home(request:Request):
     return templates.TemplateResponse('index.html',{'request':request,'message':'Welcome'})
 
 #Inyeccion de dependencias
+#Tipo 1 con metodo y libreria FastApi - Depends
 def commons_param(start_date:str, end_date:str):
     return {"start_date":start_date,"end_date":end_date}
 
-commonsDep=Annotated[dict, Depends(commons_param)]
+#Tipo 2 con Annotated de Typing
+commons_dependency=Annotated[dict, Depends(commons_param)]
+
+#Tipo 3 con Clase
+class CommonDependency:
+    def __init__(self,start_date: str, end_date: str) -> None:
+        self.start_date = start_date
+        self.end_date = end_date
 
 @app.get('/users', tags=['venta'])
-def get_users(commons: commonsDep):    #dependencia declarada con Annotated de typing
+def get_users(commons: commons_dependency):    #dependencia declarada con Annotated de typing
     return f"Users created between {commons['start_date']} and {commons['end_date']}"
 
 @app.get('/customers', tags=['venta'])
 def get_customers(commons: dict=Depends(commons_param)):    #dependencia guardada como variable diccionario
     return f"Customers created between {commons['start_date']} and {commons['end_date']}"
+
+
+@app.get('/employees', tags=['venta'])
+def get_users(commons: CommonDependency=Depends()):    #dependencia declarada con una clase (mismo nombre de variable igualada, se puede eliminar)
+    return f"Employee created between {commons.start_date} and {commons.end_date}"
+
 
 #Creacion de las rutas en MAIN
 app.include_router(prefix='/movies', router=movie_router)
