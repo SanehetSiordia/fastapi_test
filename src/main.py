0ff305ad5831d3ab6f45abfd7e52061d742f68f2
SinhueSiordia <carpediem.sinhue@gmail.com>
2025-11-17 18:00:50 -0600

    se agrega token y validacion

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 import os
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.exceptions import HTTPException
 from typing import Annotated
 from src.routers.movie_router import movie_router
 from src.utils.http_error_handler import HTTPErrorHandler
+from jose import jwt
 
 app = FastAPI()
 #Modificacion de openapi FastApi
 app.title ="HolaMundo fastAPI SSM"
 app.version= "0.0.1-SNAPSHOT"
+
+#OAuth2Password Esquema
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
 
 #Importar middleware para manejo de errores con Starlette
 app.middleware(HTTPErrorHandler) 
@@ -21,6 +27,42 @@ templates_path = os.path.join(os.path.dirname(__file__), 'templates/')
 #Carga de archivos de ruta espeficia
 app.mount('/static', StaticFiles(directory=static_path),'static')
 templates= Jinja2Templates(directory=templates_path)
+
+#Diccionario de ejemplo para usuarios y datos.
+users={
+    "Sinhue":{"username":"Sinhue","email":"sinhue@gmail.com","password":"9871231"},
+    "UsuarioX":{"username":"UsuarioX","email":"userx@gmail.com","password":"userX"},
+}
+
+#Funcionas Encoder y Decoder para Tokens Generados
+#Con JWT se suministra:
+# 1.-El diccionario con los datos para armar el token (payload).
+# 2. Una clave secreta (GUARDAR EN VARIABLES DE ENTORNO)
+# 3. El algoritmo para generar token
+def encode_token(payload: dict) -> str:
+    token = jwt.encode(payload,"my-secret",algorithm="HS256")
+    return token
+
+#El token se debe pasar en los Headers de la Ruta y no por tipo QUERY
+#Por lo que se usa Annotated
+def decode_token(token: Annotated[str,Depends(oauth2_schema)]) -> dict:
+    data=jwt.decode(token, "my-secret",algorithms=["HS256"])
+    user=users.get(data["username"])
+    return user
+
+#Url login con token
+@app.post("/token")
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user=users.get(form_data.username)
+    if not user or form_data.password!=user["password"]:
+        raise HTTPException(status_code=400, detail="Incorrect Username or Password")
+    token=encode_token({"username":user["username"],"email":user["email"]})
+    return {"access_token":token}
+
+#Ruta para obtener la informacion del usuario a la que pertenece el Token Generado.
+@app.get("/user/profile")
+def profile(my_user:Annotated[dict,Depends(decode_token)]):
+    return my_user
 
 @app.get('/', tags=['Home'])
 def home(request:Request):
